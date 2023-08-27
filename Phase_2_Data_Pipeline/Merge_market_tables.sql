28a3f32e4b3503f36f177f40978d1f799dcd5166
pratyushkarna18 <pratyushkarna18@gmail.com>
2023-08-26 18:28:13 -0700

    uploaded SQL file for data marge

---
+++ Phase_2_Data_Pipeline/Merge_market_tables.sql
@@ -0,0 +1,41 @@
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `windows-server-2019-319418.myDataset.market1`
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `windows-server-2019-319418.myDataset.market2`
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `windows-server-2019-319418.myDataset.market3`
